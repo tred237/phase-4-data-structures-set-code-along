888378a64b2e89a9eb91b6fc6d7fc3b99815f1ee
trevor dewindt <trevor.dewindt@gmail.com>
2023-06-13 20:28:03 -0600

    commit tree dc43720473e55807098913f90762ef935b72c6a3

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,35 @@
-# your code here
+require 'byebug'
+
+class MySet < Array
+    # attr_accessor :enumerable
+    # # byebug
+    def initialize(enumerable = [])
+        # byebug
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true # add a value as a key on the hash
+        self # return the updated set
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+end
+
+# set = MySet[1,2,3]
+# byebug
+# 0
